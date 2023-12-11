0a88d137b7e1d3c80149aa91641a92c0924fa9d3
Oliver Lehmann <lehmannoliver96@gmail.com>
2023-12-11 17:33:50 +0100

    fix cors bug

--- backend/fracture_risk/settings.py
+++ backend/fracture_risk/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # IMPORTANT: this must be the same as the frontend
-    "https://fracture-risk.onrender.com",
+    "https://bonoai-frontend.onrender.com/",
 ]
 
 ROOT_URLCONF = "fracture_risk.urls"
